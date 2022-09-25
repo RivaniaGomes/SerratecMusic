8a3453859a37af5d7b28d58ad34067151cf09a4d
Rivânia Gomes <rivaniagomes@gmail.com>
2022-09-25 14:38:04 -0300

    Validando a tela de login

--- js/login.js
+++ js/login.js
@@ -5,13 +5,34 @@ let formulario = {
 };
 
 //Aqui estamos escutando o evento de click
-formulario.btnentrar.addEventListener('click', () => {
-    alert("Olá!!!")
+formulario.btnentrar.addEventListener("click", () => {
+  alert("Olá!!!");
 
-    let usuario = new Usuario({
-        email: formulario.email.value,
-        senha: formulario.senha.value 
-    });
+  let usuario = new Usuario({
+    email: formulario.email.value,
+    senha: formulario.senha.value,
+  });
 
-    window.open('/artistas.html', '_self');
+  // validar se o usuario e senha podemn acessar o sistema.
+  //window.open("/artistas.html", "_self");
+  autenticar(usuario.email, usuario.senha);
 });
+
+function autenticar(email, senha) {
+  fetch("${URL_BASE}/api/login", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ user: email, password: senha }),
+  })
+    .then((response) => {
+      let token = response.headers.get("Authorization");
+      salvarToken(token);
+      direcionarTelaDeArtistas();
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("Não foi possível se autenticar");
+    });
+}
